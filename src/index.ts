30337028dd95d1c0052f5f3022985004fd7b9e87
Maycol Flores <mflores@norcul.com>
2023-06-28 12:11:35 -0600

    port updated

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ dotenv.config()
 
 import app from './server'
 
-app.listen(3001, () => {
+const port = process.env.PORT || 3001
+
+app.listen(port, () => {
     console.log("Listen on localhost:3001")
 })
